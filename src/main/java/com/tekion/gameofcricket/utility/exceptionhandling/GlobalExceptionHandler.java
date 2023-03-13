34433688b4c8ece66c5c315e0076b043ebd935fd
Abhinav Rishi <abhirishi0805@gmail.com>
2023-03-13 18:57:18 +0530

    runtime exceptions handled

--- src/main/java/com/tekion/gameofcricket/utility/exceptionhandling/GlobalExceptionHandler.java
+++ src/main/java/com/tekion/gameofcricket/utility/exceptionhandling/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.tekion.gameofcricket.utility.exceptionhandling;
 
-import com.mongodb.MongoWriteException;
 import com.tekion.gameofcricket.responsebody.GenericResponseDto;
 import com.tekion.gameofcricket.utility.enums.ResponseStatus;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,47 +16,54 @@ import java.util.NoSuchElementException;
  * This class handles all the thrown exceptions
  */
 @RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public final class GlobalExceptionHandler {
 
     @ExceptionHandler(InvalidObjectIdException.class)
     public ResponseEntity<GenericResponseDto> handleInvalidObjectIdException(InvalidObjectIdException exception) {
         exception.printStackTrace();
-        GenericResponseDto response = GenericResponseDto.builder().status(ResponseStatus.FAILED).message(exception.getTitle())
-                                                        .build();
+        GenericResponseDto response = GenericResponseDto.builder().status(ResponseStatus.FAILED)
+                                                        .message(exception.getTitle()).build();
         return ResponseEntity.badRequest().body(response);
     }
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<GenericResponseDto> handleNoSuchElementException(NoSuchElementException exception) {
         exception.printStackTrace();
-        GenericResponseDto response = GenericResponseDto.builder().status(ResponseStatus.SUCCESS).message(exception.getMessage())
-                                                        .build();
+        GenericResponseDto response = GenericResponseDto.builder().status(ResponseStatus.SUCCESS)
+                                                        .message(exception.getMessage()).build();
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(InvalidRequestBodyException.class)
     public ResponseEntity<GenericResponseDto> handleInvalidRequestBodyException(InvalidRequestBodyException exception) {
         exception.printStackTrace();
-        GenericResponseDto response = GenericResponseDto.builder().status(ResponseStatus.FAILED).message(exception.getMessage())
-                                                        .build();
+        GenericResponseDto response = GenericResponseDto.builder().status(ResponseStatus.FAILED)
+                                                        .message(exception.getMessage()).build();
         return ResponseEntity.badRequest().body(response);
     }
 
     @ExceptionHandler(InvalidDateException.class)
     public ResponseEntity<GenericResponseDto> handleInvalidDateException(InvalidDateException exception) {
         exception.printStackTrace();
-        GenericResponseDto response = GenericResponseDto.builder().status(ResponseStatus.FAILED).message(exception.getMessage())
-                                                        .build();
+        GenericResponseDto response = GenericResponseDto.builder().status(ResponseStatus.FAILED)
+                                                        .message(exception.getMessage()).build();
         return ResponseEntity.badRequest().body(response);
     }
 
-    @ExceptionHandler(MongoWriteException.class)
-    public ResponseEntity<GenericResponseDto> handleMongoWriteException(MongoWriteException exception) {
+    @ExceptionHandler(DuplicateKeyException.class)
+    public ResponseEntity<GenericResponseDto> handleDuplicateKeyException(DuplicateKeyException exception) {
         exception.printStackTrace();
-        GenericResponseDto response = GenericResponseDto.builder().status(ResponseStatus.FAILED).build();
-        if (exception.getCode() == 11000) {
-            response.setMessage("This name has already been used. Try something different");
-        }
+        GenericResponseDto response = GenericResponseDto.builder().status(ResponseStatus.FAILED).message(
+                "This name has already been used. Try something different").build();
         return ResponseEntity.ok().body(response);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<GenericResponseDto> handleRemainingExceptions(RuntimeException exception) {
+        exception.printStackTrace();
+        GenericResponseDto response = GenericResponseDto.builder().status(ResponseStatus.FAILED)
+                                                        .message("Some error occurred!").build();
+        return ResponseEntity.internalServerError().body(response);
     }
 }
